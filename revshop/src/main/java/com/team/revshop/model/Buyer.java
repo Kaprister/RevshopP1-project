8df538b65b5dc9b666eab9b7d3ce20a4c0a45426
Kaprister <sushantkapri@gamil.com>
2024-10-18 19:34:33 +0530

    add model

--- revshop/src/main/java/com/team/revshop/model/Buyer.java
+++ revshop/src/main/java/com/team/revshop/model/Buyer.java
@@ -1,4 +1,5 @@
 package com.team.revshop.model;
 
 public class Buyer {
+
 }
--- revshop/src/main/java/com/team/revshop/model/Order.java
+++ revshop/src/main/java/com/team/revshop/model/Order.java
@@ -1,8 +1,6 @@
 package com.team.revshop.model;
 
-import jakarta.persistence.Entity;
-
-@Entity
 public class Order {
+
 
 }
